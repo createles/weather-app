8ddc39b6cd319b73d666a0a3883fb2873e8f3696
createlsworklappy <justinecastillo7@gmail.com>
2025-07-01 14:42:07 +0900

    Initial commit
    Initial code;
    -set base html outline and styling
    - implement button click behavior and API call to visual crossing

---
+++ script.js
@@ -0,0 +1,51 @@
+const searchBar = document.querySelector("#searchBar");
+const searchBtn = document.querySelector("#search");
+const weatherIconElement = document.querySelector("#weather-icon-element");
+let selectedLocation = null;
+
+const visualCrossingIconMap = {
+  'snow': 'ac_unit',
+  'snow-showers-day': 'cloudy_snowing',
+  'snow-showers-night': 'cloudy_snowing',
+  'thunder-rain': 'thunderstorm',
+  'thunder-showers-day': 'thunderstorm',
+  'thunder-showers-night': 'thunderstorm',
+  'rain': 'rainy',
+  'showers-day': 'rainy',
+  'showers-night': 'rainy',
+  'fog': 'foggy',
+  'wind': 'air',
+  'cloudy': 'cloudy',
+  'partly-cloudy-day': 'partly_cloudy_day',
+  'partly-cloudy-night': 'partly_cloudy_night',
+  'clear-day': 'sunny',
+  'clear-night': 'dark_mode',
+  'default': 'thermostat'
+};
+
+async function searchLocation(place) {
+  try {
+    const encodedUserLocation = encodeURIComponent(place);
+    const locationQuery = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodedUserLocation}?unitGroup=metric&&include=days,hours,current&key=7LWNL9SWFWLZP9ARRA5CXXUN7&contentType=json`
+    );
+    if (!locationQuery.ok) {
+      throw new Error("Error fetching data from the server.");
+    }
+    const locationQueryParsed = await locationQuery.json();
+
+    console.log(locationQueryParsed);
+    selectedLocation = locationQueryParsed;
+    return selectedLocation;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+searchBtn.addEventListener("click", async () => {
+    const locationData = await searchLocation(searchBar.value);
+    const iconName = locationData.currentConditions.icon;
+    const googleIconName = visualCrossingIconMap[iconName] || visualCrossingIconMap["default"];
+    weatherIconElement.textContent = googleIconName;
+})
+
